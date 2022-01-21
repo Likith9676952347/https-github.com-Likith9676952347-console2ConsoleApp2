a5b434fdf4cc84ec4a8e969b409ed2b6a08d30b1
Likith9676952347 <likithkumarreddy143@gmail.com>
2022-01-21 22:07:29 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            string departmentName = emp.GetDeptName();
+            string employeeName = emp.GetEmpName();
+
+            Console.WriteLine(employeeName + "belongs to " + departmentName);
+        }
+    }
+}
---
+++ ConsoleApp2/department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+     public class department
+    {
+        public string GetDeptName()
+        {
+            return "IT Department";
+        }
+    }
+}
